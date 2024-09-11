f795284130015eb03b1e2383ca4b9c5a914abdd5
Abdulaziz Hassan kehinde <abdulazizhassankehinde19@gmail.com>
2024-09-11 21:51:19 +0100

    init: first push

---
+++ index.js
@@ -0,0 +1,114 @@
+class Node {
+    constructor(value){
+        this.value = value;
+        this.next = null;
+    }
+}
+ 
+class LinkedList {
+    constructor(value) {
+        const newNode = new Node(value);
+        this.head = newNode;
+        this.tail = this.head;
+    }
+
+    printList() {
+        let temp = this.head;
+        while (temp !== null) {
+            console.log(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    getHead() {
+        if (this.head === null) {
+            console.log("Head: null");
+        } else {
+            console.log("Head: " + this.head.value);
+        }
+    }
+
+    getTail() {
+        if (this.tail === null) {
+            console.log("Tail: null");
+        } else {
+            console.log("Tail: " + this.tail.value);
+        }
+    }
+
+    makeEmpty() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+ 
+    push(value) {
+        const newNode = new Node(value);
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+    }
+    
+  findKthFromEnd(k) {
+    if (!this.head || k <= 0) return null; // Edge case: if list is empty or k is invalid
+
+    let fast = this.head;
+    let slow = this.head;
+    
+    // Move the fast pointer k nodes ahead
+    for (let i = 0; i < k; i++) {
+        if (!fast) return null; // If k is greater than the length of the list
+        fast = fast.next;
+    }
+
+    // Move both pointers until the fast pointer reaches the end
+    while (fast) {
+        fast = fast.next;
+        slow = slow.next;
+    }
+
+    // The slow pointer is now at the k-th node from the end
+    console.log("K-th node from end:", JSON.stringify(slow));
+    return slow;
+}
+
+}
+
+
+
+let myLinkedList = new LinkedList(1);
+myLinkedList.push(2);
+myLinkedList.push(3);
+myLinkedList.push(4);
+myLinkedList.push(5);
+
+console.log("Original list:");
+myLinkedList.printList();
+
+const k = 2;
+const kthNodeFromEnd = myLinkedList.findKthFromEnd(k);
+
+console.log(`\n${k}th node from the end:`);
+if (kthNodeFromEnd) {
+    console.log(kthNodeFromEnd.value);
+} else {
+    console.log("Not found");
+}
+
+
+/*
+    EXPECTED OUTPUT:
+    ----------------
+    Original list:
+    1
+    2
+    3
+    4
+    5
+    2th node from the end:
+    4
+*/
